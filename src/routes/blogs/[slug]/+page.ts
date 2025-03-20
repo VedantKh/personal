94eb6875b6026eaa093afa221688f511f0915a95
Vedant Khanna <vedantkhanna@gmail.com>
2025-03-20 04:16:37 -0700

    Checkpoint: File based routing set up with slug load function in +page.ts

---
+++ src/routes/blogs/[slug]/+page.ts
@@ -0,0 +1,14 @@
+// src/routes/blog/[slug]/+page.ts
+import type { PageLoad } from './$types';
+
+export const load = (async ({ params }) => {
+	const post = await import(`../${params.slug}.md`);
+	const { title, date } = post.metadata;
+	const content = post.default;
+
+	return {
+		content,
+		title,
+		date
+	};
+}) satisfies PageLoad;
