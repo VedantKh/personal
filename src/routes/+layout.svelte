9275b6c95d65f82302e3abd87094f4060a580b04
Vedant Khanna <vedantkhanna@gmail.com>
2025-03-20 03:49:20 -0700

    markdown to html working in /uses

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,8 @@
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import '$lib/styles/style.scss'
+	import '$lib/styles/style.scss';
+	import 'prismjs/themes/prism.css';
 	let { children } = $props();
 </script>
 
