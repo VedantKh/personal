6c3bc757c1d463df2a1303baa301f7f1318335f1
Vedant Khanna <vedantkhanna@gmail.com>
2025-08-17 13:08:26 -0700

    add twitter link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -67,6 +67,24 @@
 						</svg>
 					</a>
 				</li>
+				<li>
+					<a href="https://x.com/vedant__khanna" aria-label="Twitter/X">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path d="M4 4l11.733 16h4.267l-11.733 -16z"></path>
+							<path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path>
+						</svg>
+					</a>
+				</li>
 			</ul>
 		</nav>
 
