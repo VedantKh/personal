8fe17a91ec9247e590a72246b6d6414ccaa89135
Vedant Khanna <vedantkhanna@gmail.com>
2025-08-16 18:59:49 -0700

    test link

--- src/routes/writings/[slug]/+page.ts
+++ src/routes/writings/[slug]/+page.ts
@@ -1,14 +1,20 @@
 // src/routes/blog/[slug]/+page.ts
 import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ params }) => {
-	const post = await import(`../${params.slug}.md`);
-	const { title, date } = post.metadata;
-	const Content = post.default;
+	try {
+		const post = await import(`../${params.slug}.md`);
+		const { title, date } = post.metadata;
+		const Content = post.default;
 
-	return {
-		title,
-		date,
-		Content
-	};
+		return {
+			title,
+			date,
+			Content
+		};
+	} catch (e) {
+		console.error(`Failed to load post: ${params.slug}`, e);
+		throw error(404, `Could not find ${params.slug}`);
+	}
 }) satisfies PageLoad;
