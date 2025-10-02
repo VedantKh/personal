2753ff404dd4416387c005d8c68d2daf2bae8a8b
Vedant Khanna <vedantkhanna@gmail.com>
2025-10-02 12:06:50 -0700

    ok going to do something useful now

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -114,9 +114,7 @@
 		</nav>
 
 		<p class="inspired">
-			<a
-				href="https://www.youtube.com/watch?v=SqSuRdkglxM&t=6s"
-				target="_blank">Inspired by Tron</a
+			<a href="https://www.youtube.com/watch?v=SqSuRdkglxM&t=6s" target="_blank">Inspired by <span class="tron">Tron</span></a
 			>
 		</p>
 
@@ -160,6 +158,9 @@
 
 		a {
 			display: inline;
+		}
+		.tron {
+			color: #ff8c00;
 		}
 	}
 
